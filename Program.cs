2f49bdffdc8b8e6a27c0a4c0a9681d475bd293bf
MM-2103 <mohsen.menem@protonmail.com>
2025-09-09 16:29:12 +0200

    Program.cs: updated with basic EnergyCalc class for now

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class EnergyCalc
+{
+    public static void Main()
+    {
+        const double fridge = 200;
+        const double microwave = 300;
+        const double heater = 100;
+
+        var total_energy = fridge + microwave + heater;
+
+        Console.WriteLine("The Energy used in total is {0} kwh", total_energy);
+    }
+}
