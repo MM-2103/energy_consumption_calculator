43622039354b77e5c3fc8a050e2e8f7091c512c2
MM-2103 <mohsen.menem@protonmail.com>
2025-09-10 01:02:53 +0200

    Program.cs: divide by 1000 to convert w to kw

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@
         double item_1_power = int.Parse(Console.ReadLine());
         Console.WriteLine("What is the average number of hours that your {0} for per day", item_1);
         double item_1_hours = int.Parse(Console.ReadLine());
-        double item_1_consumption = item_1_power * item_1_hours;
+        double item_1_consumption = item_1_power * item_1_hours / 1000;
         Console.WriteLine("Daily consumption of {0} per day is {1}kwh\n", item_1, item_1_consumption);
 
         Console.WriteLine("Enter the name of item_2");
@@ -19,7 +19,7 @@
         double item_2_power = int.Parse(Console.ReadLine());
         Console.WriteLine("What is the average number of hours that your {0} for per day", item_2);
         double item_2_hours = int.Parse(Console.ReadLine());
-        double item_2_consumption = item_2_power * item_2_hours;
+        double item_2_consumption = item_2_power * item_2_hours / 1000;
         Console.WriteLine("Daily consumption of {0} per day is {1}kwh\n", item_2, item_2_consumption);
 
         Console.WriteLine("Enter the name of item_3");
@@ -28,7 +28,7 @@
         double item_3_power = int.Parse(Console.ReadLine());
         Console.WriteLine("What is the average number of hours that your {0} for per day", item_3);
         double item_3_hours = int.Parse(Console.ReadLine());
-        double item_3_consumption = item_3_power * item_3_hours;
+        double item_3_consumption = item_3_power * item_3_hours / 1000;
         Console.WriteLine("Daily consumption of {0} per day is {1}kwh\n", item_3, item_3_consumption);
 
         var total_energy = item_1_consumption + item_2_consumption + item_3_consumption;
